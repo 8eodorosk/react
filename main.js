7ec8d86d7badea12944e884118a226c456ad84c4
Thodoris Koutsis <8eodorosk@gmail.com>
2019-03-03 16:33:05 +0200

    learning project

---
+++ main.js
@@ -0,0 +1,23 @@
+// foreach
+
+// const fruits = ["Apples", "Oranges", "Grapes"];
+
+// fruits.forEach((fruit, index) => console.log(fruit));
+
+//map
+// const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+
+// console.log(singleFruit);
+
+//filter
+
+// const people = [
+//   { id: 1, name: "karen" },
+//   { id: 2, name: "Sharon" },
+//   { id: 3, name: "Bob" }
+// ];
+
+// const people2 = people.filter(person => person.id != 3);
+// console.log(people2);
+
+// spread operator
