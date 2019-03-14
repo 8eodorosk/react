4daeaab87dbf33a5b47a13c2da568104bf248c89
Koutsis <PI9278@piag.com>
2019-03-14 16:53:06 +0200

    added form

---
+++ my-app/src/components/contacts/Contact.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Consumer } from '../../context';
+
+class Contact extends Component {
+  state = {
+    showContacInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({ showContacInfo: !this.state.showContacInfo });
+  };
+
+  onDeleteClick = (id, dispatch) => {
+    dispatch({
+      type: 'DELETE_CONTACT',
+      payload: id
+    })
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContacInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}
+                <i
+                  className="fas fa-sort-down"
+                  // prepei na kanw to bind ama de xrisimopoiisw arrow function
+                  // stis custom methodous poy ftiaxnw
+                  // onClick={this.onShowClick.bind(this)}
+                  onClick={() =>
+                    this.setState({ showContacInfo: !this.state.showContacInfo })
+                  }
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
+              {showContacInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          )
+        }}
+      </Consumer>
+
+
+
+
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
